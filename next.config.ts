a47d8670006a1d66b5c42b5be24504da3b04a630
rangaraj.s <rangaraj.sam@gmail.com>
2025-04-21 23:36:09 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     remotePatterns: [
       {
@@ -18,10 +22,7 @@ const nextConfig: NextConfig = {
         permanent: true
       },
     ]
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  }
 };
 
 export default nextConfig;
